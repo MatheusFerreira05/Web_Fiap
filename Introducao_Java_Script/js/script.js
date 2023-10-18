95fe3a22a51c0b326cf150fc818d78b0899f32b9
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-10-18 17:42:41 -0300

    Introdução ao Java Script

---
+++ Introducao_Java_Script/js/script.js
@@ -0,0 +1,20 @@
+alert('Ola Mundo!!!');
+document.write("Esta é a nossa primeira impressão na tela")
+
+let nome; // Cria uma variável com valor indefinido
+let disciplina = null; //Cria uma variável vazia
+let texto = "Este é um exemplo";
+
+let carro = {
+    cor: "preto",
+    numPortas: 4,
+    tipo: "sedan",
+    modelo: "Onix",
+    marca: "GM",
+    correr: function(){
+        alert("Estou correndo!!!")
+    }
+}
+
+console.log(carro.modelo)
+carro.correr()
