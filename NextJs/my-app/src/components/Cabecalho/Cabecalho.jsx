93792a991d4d154f2efa34a1f49262383a9b39aa
Matheus da Silva Ferreira <56132266+MatheusFerreira05@users.noreply.github.com>
2023-10-20 22:23:01 -0300

    Aula Next

---
+++ NextJs/my-app/src/components/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import "./Cabecalho.scss";
+
+
+export default function Cabecalho() {
+  return (
+    <header className='cabecalho'>
+        <nav>
+          <Link href="/produtos/calca">CALCA</Link>
+          <Link href="/produtos/camisa">CAMISA</Link>
+          <Link href="/produtos/meia">MEIA</Link>
+          <Link href="/produtos/tenis">TÊNIS</Link>
+          <Link href="/tabela/queijo">QUEIJOS</Link>
+        </nav>
+    </header>
+  )
+}
